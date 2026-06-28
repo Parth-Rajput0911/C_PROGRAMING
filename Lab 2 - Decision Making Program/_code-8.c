2e0f2917025e52235681ba06b4cf9cbc8cd3cd9c
Parth-Rajput0911 <parthvikramsingh100@gmail.com>
2026-06-28 23:20:46 +0530

    Add program to calculate the absolute value of a number

---
+++ Lab 2 - Decision Making Program/_code-8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(n < 0)
+        n = -n;
+
+    printf("Absolute Value = %d", n);
+
+    return 0;
+}
