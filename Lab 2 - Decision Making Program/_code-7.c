15c1d317049e20d1d18bb9a89b953e91dd70139d
Parth-Rajput0911 <parthvikramsingh100@gmail.com>
2026-06-28 23:14:33 +0530

    Add program to check if a character is an alphabet

---
+++ Lab 2 - Decision Making Program/_code-7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+
+    if(ch >= 'A' && ch <= 'Z')
+        printf("Uppercase Letter");
+    else if(ch >= 'a' && ch <= 'z')
+        printf("Lowercase Letter");
+    else
+        printf("Not an Alphabet");
+
+    return 0;
+}
